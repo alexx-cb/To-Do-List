3a751421f3657e252ecf4a9902c4d2aac519c015
Alejandro Cabrera <alejandrocabrerabarea@gmail.com>
2025-01-23 08:32:38 +0100

    Ocultando credenciales

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,14 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore} from 'firebase/firestore'
 import { getAuth, GithubAuthProvider, signInWithPopup } from 'firebase/auth';
-
+import { conexion } from './firebaseApiKey';
 
 
 
 
 // ... other firebase imports
 
-export const firebaseApp = initializeApp({
-  // your application settings
-
-    apiKey: "AIzaSyBMlcrRadZ0jlTNqMe8-NkgRw0BkDwJwWQ",
-    authDomain: "testfrclase.firebaseapp.com",
-    projectId: "testfrclase",
-    storageBucket: "testfrclase.firebasestorage.app",
-    messagingSenderId: "357066239918",
-    appId: "1:357066239918:web:603fefeff23d728b4ba23f",
-    measurementId: "G-QML04LB5ZL"
-})
+export const firebaseApp = initializeApp(conexion);
 const authGit = getAuth(firebaseApp);
 const provider = new GithubAuthProvider();
 
